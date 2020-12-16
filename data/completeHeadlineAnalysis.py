b578a5d899c5074f4f3a6f3bc087cf5ba3a16c72
cat-bevan <70996599+cat-bevan@users.noreply.github.com>
2020-12-16 08:45:23 -0700

    Update completeHeadlineAnalysis.py

--- data/completeHeadlineAnalysis.py
+++ data/completeHeadlineAnalysis.py
@@ -1,16 +1,19 @@
 import csv
-currCSV = 'torontoStar.csv'
+#This script takes the full .csv files with upwards of 2 million headlines per news source, and scrapes them for trans-related headlines
+
+currCSV = 'torontoStar.csv' #initialize current .csv to strip
 destinationCSV = 'torontoStarDF.csv'
-dateRange = ' '
+dateRange = ' ' #something optional I was playing with. Used this to extract those 2020-only .csv
 currMedia = 'Toronto Star'
 
+#all strings to search for are here. These were everything that came to mind for trans-related headlines. Very easy to re-purpose to any other set of words
 searchStrings = ['transgender', 'transwoman', 'transman', 'tranny', 'trannie', 'transsexual', 'transexual', 'transperson', 'trans woman', 'trans man', 'trans person', 'transgendered', 'non-binary']
 
-totalHeadlines = 0
+totalHeadlines = 0 #for calculating percentage of trans-related headlines
 transHeadlines = 0
 results = []
 
-def rowCreate(csvFile):
+def rowCreate(csvFile): #extract by row
     with open(csvFile, newline = '') as file:
         csvreader = csv.reader(file, delimiter = '|')
         rows = [row for row in csvreader]
@@ -26,11 +29,11 @@ def isolateTransHeadlines(rows):
         if currDate.find(dateRange) != -1:
             totalHeadlines = totalHeadlines + 1
             currHeadline = row[2]
-            correctHeadline = currHeadline.lower()
+            correctHeadline = currHeadline.lower() #convert to lowercase to match to search strings
             for string in searchStrings:
                 results.append(string in correctHeadline)
                 output = any(results)
-                if output == True:
+                if output == True: #check if there was any matches
                     transHeadlines = transHeadlines + 1
                     currDate = row[1]
                     output = False
